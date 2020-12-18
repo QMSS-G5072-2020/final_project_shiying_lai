e2285b598bd06d9d8b3730b75ab1be572ce0c5c1
shiyinglai <sl4849@columbia.edu>
2020-12-17 19:27:26 -0500

    update test

--- tests/test_final_project_shiyinglai.py
+++ tests/test_final_project_shiyinglai.py
@@ -1,5 +1,31 @@
 from final_project_shiyinglai import __version__
 from final_project_shiyinglai import final_project_shiyinglai
+import os
+import pandas as pd
+import urllib3
+import json
+from PIL import Image
+import requests
+from io import BytesIO
+from datetime import date
+import matplotlib. pyplot as plt 
+api_key = os.environ.get("PRIVATE_API_KEY")
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_search_artwork_by_culture():
+    actual=type(search_artwork_by_culture(apikey=api_key,culture="British",page="1"))
+    expected = pd.core.frame.DataFrame
+    assert actual == expected 
+def test_find_artist():
+    actual=type(find_artist(name="Cronin",apikey=api_key))
+    expected = pd.core.frame.DataFrame
+    assert actual == expected 
+def test_exhibition_explorer():
+    actual=type(exhibition_explorer(apikey=api_key,exhibition="Botticelli's Witness:  Changing Style in a Changing Florence"))
+    expected = pd.core.frame.DataFrame
+    assert actual == expected 
+    
+   
